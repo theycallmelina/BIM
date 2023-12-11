d24e8764bea36cd205b96239200d8c6347c983b1
Алина <153538872+theycallmelina@users.noreply.github.com>
2023-12-11 20:26:21 +0300

    Add files via upload

---
+++ Лабораторная работа 1/task_4.py
@@ -0,0 +1,10 @@
+users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
+
+# TODO Добавьте словарь и замените в нем нулевые значения статисчикой посещений
+statistic = {
+    "Общее количество": 0,
+    "Уникальные посещения": 0
+}
+statistic["Общее количество"] = len(users)
+statistic["Уникальные посещения"] = len(set(users))
+print(statistic)
